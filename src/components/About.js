1bc24c7ccd62e36c1a03225495e9c96a52b06bdf
VMammedov <vusal.memmedli25official@gmail.com>
2022-05-24 14:44:33 +0400

    Init

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import {Container,Row,Col} from 'reactstrap'
+export default function About(){
+    return(
+        <>
+        <Container>
+            <Row>
+                <Col xs={12}>
+                    <h4 className='text-success mt-4'>About</h4>
+                </Col>
+            </Row>
+        </Container>
+        </>
+    )
+}
